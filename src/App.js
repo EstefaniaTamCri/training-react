595eaf6f17bb7a2440e70e674d6df58c9926b3e4
EstefaniaTamCri <estefania_92_9@hotmail.com>
2023-05-09 20:09:21 +0200

    cambiando ejercicio cards a otra rama

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Avengers from "./components/User/Avengers";
 import Terminator from "./components/Character/Terminator";
 import Split from "./components/Wrapper/SplitPane";
+import Cards from "./components/Cards/Cards";
+
 const App = () => {
   return (
     <>
       <Split left={<Terminator />} right={<Avengers />} />
+      {}
+      {/* <Cards /> */}
     </>
   );
 };
