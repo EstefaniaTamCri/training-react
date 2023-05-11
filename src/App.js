64b788d624e6940ce58e495ede67c7a9697d7b80
EstefaniaTamCri <estefania_92_9@hotmail.com>
2023-05-11 20:34:16 +0200

    creado rama para empezar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./App.css"
-import Title from "./components/HeaderTitle"
-import Paragraph from "./components/FillerParagraph"
-
+// import Title from "./components/HeaderTitle"
+// import Paragraph from "./components/FillerParagraph"
+import Expenses from "./components/Expenses";
 const App = () => {
   return (
     <div className="main">
-      <Title />
-      <Paragraph />
-      <Paragraph />
-      <Paragraph />
+      {/* <Title />
+      <Paragraph /> */}
+     <h1>Transaction</h1>
 
     </div>
   );
