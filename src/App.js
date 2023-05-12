0d10597bef4de76f7f5f3b0396df5b0af0223873
EstefaniaTamCri <estefania_92_9@hotmail.com>
2023-05-12 16:00:46 +0200

    solo rama creada

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import "./App.css"
-import Title from "./components/HeaderTitle"
-import Paragraph from "./components/FillerParagraph"
-
+import Search from "./components/UI/SearchForm"
+import Movies from "./components/Movies/Movies"
+import { useState, useEffect } from "react";
 const App = () => {
+  
   return (
-    <div className="main">
-      <Title />
-      <Paragraph />
-      <Paragraph />
-      <Paragraph />
-
-    </div>
+    <>
+   
+    <header>
+      <Search />
+    </header>
+    <main>
+      <Movies />
+    </main>
+    </>
   );
 };
 
