5d4fc05d7e3aca5acdf08fe263f0f844930e3394
EstefaniaTamCri <estefania_92_9@hotmail.com>
2023-05-14 16:16:50 +0200

    listo para empezar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import Cards from "./components/Cards/Card";
-// import Dogs from "./components/Dogs/Dogs";
-import Wrapper from "./components/Wrapper/Wrapper";
+
 const App = () => {
   return (
     <>
-      <Wrapper >
-      {/* <Dogs /> */}
-        <Cards />
-      </Wrapper>
+     
     </>
   );
 };
